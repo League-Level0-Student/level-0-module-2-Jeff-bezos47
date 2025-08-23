52143befb3eda8d6d2a1ebf931bfd0bcfde98624
league <bobby>
2025-08-23 11:30:25 -0700

    goonerours

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -9,21 +9,44 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
-		
+
 		Random randomMaker = new Random();
 		
-		int randomNumber = randomMaker.nextInt(5);
 		
+		for(int dino=0; dino<10; dino++) {
+
+		int randomNumber = randomMaker.nextInt(4);
+
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
 		if(randomNumber == 0) {
-			JOptionPane.showMessageDialog(null, "Good jeb");
+			JOptionPane.showMessageDialog(null, "Goooood, very gooood");
 		}
-		else if
-		
+		else if(randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "EUPHRATES RIVER");
+		}
+		else if(randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "NICE SHIRT");
+		}
+		else if(randomNumber == 3) {
+			String something = JOptionPane.showInputDialog("Do you play Grow A Garden?? Yes or no?????");
+			if(something.equalsIgnoreCase("Yes")) 
+				JOptionPane.showMessageDialog(null, "You deserve a compliment: good jeb");
+
+
+
+			else {
+				JOptionPane.showMessageDialog(null, "You  don't deserve a compliment >:( SHOO, SHOoOO");
+			}
+		}
+		}
+
+
+
+
 		// 2. Repeat all the code above 10 times
-		
+
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,32 +6,45 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
+	public static void main(String[] args) {
 
-	// 2. Get the user to enter a question for the 8 ball to answer
-	
 
-	// 3. Make a variable and initialize it to a random number.
-	//     ** You will need to make a random object!
-	//     Limit the random numbers to be between 0 and 3
+		// 2. Get the user to enter a question for the 8 ball to answer
+		String something = JOptionPane.showInputDialog("Ask a question");
 
-	
-	// 4. If the random number is 0
+		// 3. Make a variable and initialize it to a random number.
+		//     ** You will need to make a random object!
+		//     Limit the random numbers to be between 0 and 3
+		Random randomMaker = new Random();
+		int ran = randomMaker.nextInt(4);
 
-	// -- tell the user "Yes"
+		// 4. If the random number is 0
+		if(ran == 0) {
+			JOptionPane.showMessageDialog(null, "yuh");
+		}
+		// -- tell the user "Yes"
 
-	// 5. If the random number is 1
+		// 5. If the random number is 1
+		if(ran == 1) {
+			JOptionPane.showMessageDialog(null, "nuh");
+		}
+		// -- tell the user "No"
 
-	// -- tell the user "No"
+		// 6. If the random number is 2
+		if(ran == 2) {
+			JOptionPane.showMessageDialog(null, "ausk guguhl");
+		}
+		// -- tell the user "Maybe you should ask Google?"
 
-	// 6. If the random number is 2
-
-	// -- tell the user "Maybe you should ask Google?"
-
-	// 7. If the random number is 3
-
-	// -- write your own answer
-
+		// 7. If the random number is 3
+		if(ran == 3) {
+			JOptionPane.showMessageDialog(null, "Iuh uhnuhstluh duhnt knuh");
+			// -- write your own answer
+		}
+	}
 }
